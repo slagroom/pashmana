f21499d0ccb3932931c0f05ab3be574ec306141b
slagroom <sl4gr00m@gmail.com>
2018-10-10 22:53:02 -0300

    Create Entry struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+extern crate chrono;
+
+use chrono::{DateTime, FixedOffset};
+
+struct Entry {
+    name: String,
+    domain: String,
+    username: String,
+    password: String,
+    password_pattern: String, // TODO:TS Reverse Regex
+    created_date: DateTime<FixedOffset>
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
